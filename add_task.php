b1795bf4d216803c2cd31d898d7ee8f54b183c2a
Gusdicnx <96022010+Gusdicnx@users.noreply.github.com>
2025-05-16 13:53:58 +0100

    Update add_task.php
    Adicionado if que vê se a task está vazia e lança uma mensagem de erro

--- add_task.php
+++ add_task.php
@@ -1,8 +1,12 @@
 
 <?php
-$connection = mysqli_connect("localhost", "root", "yourpassword", "todo_app");
+$connection = mysqli_connect("localhost", "root", "", "todo_app");
 $description = $_POST['task'];
 $query = "INSERT INTO tasks (description) VALUES ('$description')";
 mysqli_query($connection, $query);
 header("Location: index.php");
+
+if (empty($_POST['task']) === '') {
+        throw new Exception("Erro: A tarefa não pode estar vazia.");
+    }
 ?>
